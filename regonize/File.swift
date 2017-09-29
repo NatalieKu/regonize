d12689642c6b5a7708295c30e77f7916d9d22599
MEI KU <natalie@MEIde-MacBook-Air.local>
2017-09-30 00:17:34 +0800

    initial

---
+++ regonize/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  regonize
+//
+//  Created by MEI KU on 2017/9/25.
+//  Copyright © 2017年 Natalie KU. All rights reserved.
+//
+
+struct CaptionOption {
+    let emoji: String
+    let caption: String
+}
+
